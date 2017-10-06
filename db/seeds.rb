9e2c6f825fb3b2c062797321d35a07a422fd9d93
LouiseReid <l_reid@hotmail.co.uk>
2017-10-06 13:51:13 +0100

    app specs met

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,16 +4,30 @@ require_relative('../models/tickets')
 
 require('pry-byebug')
 
+Ticket.delete_all()
+Customer.delete_all()
+Film.delete_all()
+
 customer1 = Customer.new({'name' => 'Louise', 'funds' => 20})
 customer2 = Customer.new({'name' => 'Steve', 'funds' => 25})
 customer3 = Customer.new({'name' => 'Jo', 'funds' => 15})
 customer4 = Customer.new({'name' => 'Alanna', 'funds' => 10})
+
+customer1.save()
+customer2.save()
+customer3.save()
+customer4.save()
 
 
 film1 = Film.new({'title' => 'IT', 'price' => 8})
 film2 = Film.new({'title' => 'T2', 'price' => 5})
 film3 = Film.new({'title' => 'The Lion King', 'price' => 3})
 film4 = Film.new({'title' => 'Dunkirk', 'price' => 5})
+
+film1.save()
+film2.save()
+film3.save()
+film4.save()
 
 ticket1 = Ticket.new({'customer_id' => customer1.id,
                       'film_id' => film1.id})
@@ -23,3 +37,11 @@ ticket3 = Ticket.new({'customer_id' => customer3.id,
                       'film_id' => film3.id})
 ticket4 = Ticket.new({'customer_id' => customer4.id,
                       'film_id' => film4.id})
+
+ticket1.save()
+ticket2.save()
+ticket3.save()
+ticket4.save()
+
+binding.pry
+nil
---
+++ models/customers.rb
@@ -0,0 +1,55 @@
+require_relative("../db/sql_runner")
+
+class Customer
+
+  attr_reader :id
+  attr_accessor :name, :funds
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @name = options['name']
+    @funds = options['funds'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO customers (name, funds) VALUES ($1, $2) RETURNING id;"
+    values = [@name, @funds]
+    customer = SqlRunner.run(sql, values).first
+    @id = customer['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM customers;"
+    values = []
+    SqlRunner.run(sql, values)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM customers;"
+    values = []
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM customers WHERE id = $1;"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def update()
+    sql = "UPDATE customers SET (name, funds) = ($1, $2)
+      WHERE id = $3;"
+    values = [@name, @funds, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def film_booked()
+    sql = "SELECT films.* FROM films
+    INNER JOIN tickets
+    ON tickets.film_id = films.id WHERE customer_id = $1"
+    values = [@id]
+    film_booked = SqlRunner.run(sql, values)
+    return film_booked.map { |film| Film.new(film)  }
+  end
+
+end
